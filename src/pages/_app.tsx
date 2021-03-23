5fe7543f5a590abe03454fa281c89e2087bae5bc
Marisa Clardy <marisa@clardy.eu>
2021-03-22 22:55:21 -0500

    Include tailwind.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
